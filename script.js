f718bec0fac94958ef0fc4de50ef91daf7348019
abdiweli hussein <abdiwelih006@gmail.com>
2026-02-17 07:27:31 +0300

    change

--- script.js
+++ script.js
@@ -66,6 +66,25 @@ document.addEventListener("DOMContentLoaded", function () {
 
         });
     });
+// ================================
+// CONTACT FORM VALIDATION
+// ================================
+let contactForm = document.getElementById("contactForm");
+
+contactForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let name = document.getElementById("name").value.trim();
+  let email = document.getElementById("email").value.trim();
+  let message = document.getElementById("message").value.trim();
+
+  if (name === "" || email === "" || message === "") {
+    alert("⚠️ Please fill in all fields!");
+  } else {
+    alert("✅ Message sent successfully! Thank you for contacting TastyBite 🍔");
+    contactForm.reset();
+  }
+});
 
 
 
